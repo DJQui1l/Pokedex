37b7d8459061dd5564a7c750608ca542e0f60272
Darian Jarvis <djarvis@nyc.yearup.org>
2019-11-18 16:57:07 -0500

    Added more title sequences animations in js. Working on the api for the pokedex by following api tutorial.

--- master.js
+++ master.js
@@ -13,8 +13,12 @@ class Trainer {
     return this.team
   }
   get(name){
-    // for (let i = 0; i < this.team.length; i++)
-    return this.team
+    for (let i = 0; i < 5;i++){
+      if (this.team[i] == name){
+        return this.team[i]
+      }
+    }
+
   }
 }
 //
@@ -30,10 +34,11 @@ class Pokemon {
 
 
 
-
+// ------------ ANIMATIONS ------------------------------
 function start(){
   let clicklightning = document.getElementById('lightning-bolt');
   let clicktitle = document.getElementById('title');
+  let clickgrid = document.getElementById('container-grid');
 
   clicklightning.addEventListener("click", function() {
 
@@ -42,9 +47,20 @@ function start(){
     setTimeout(() => {
 			clicklightning.style.display = "none";
       clicktitle.style.display = "block"
-      clicktitle.style.animation = "fade-in 1s ease-in";
-			console.log("it works")
+      clicktitle.style.animation = "fade-in 1s ease-in, scale 4s ease-out";
+
+
+			// console.log("it works")
 		},1004)
+    setTimeout(() =>{
+        clicktitle.style.animation = "fade-out 1s ease-in, scale 4s ease-out";
+    },4000)
+    setTimeout(() =>{
+        clicktitle.style.display = "none";
+        clickgrid.style.display = "none";
+
+    },5000)
+
   })
 }
 
@@ -53,3 +69,29 @@ start()
 let t1 = new Trainer('Red')
 let poke1 = new Pokemon('bulbasaur')
 let poke2 = new Pokemon('squirtle')
+
+//
+// // -----------API STUFF--------------
+//
+// // Create a request variable and assign a new XMLHttpRequest object to it.
+// var request = new XMLHttpRequest()
+//
+// // Open a new connection, using the GET request on the URL endpoint
+// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+//
+// request.onload = function() {
+//   // Begin accessing JSON data here
+//   var data = JSON.parse(this.response)
+//
+//   if (request.status >= 200 && request.status < 400){
+//   data.forEach(movie => {
+//   // Log each movie's title
+//   console.log(movie.title)
+// })
+// } else {
+//     console.log('error')
+//   }
+// }
+//
+// // Send request
+// request.send()
