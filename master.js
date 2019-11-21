4fe14f51903e65b6a0033a87fbb3a01ab2225739
DJQui1l <darianjarvis17@gmail.com>
2019-11-21 00:03:41 -0500

    got equipPokemon_ID() function to work. I still need to push my pokemon objects into my team. writing this.team.push(chosenPoke) doesn't work as 'this' kept reffering to the xhttps variable and not my Trainer object's team array. I need assistance.

--- master.js
+++ master.js
@@ -1,3 +1,4 @@
+
 class Trainer {
   constructor(name) {
     this.name = name
@@ -5,29 +6,46 @@ class Trainer {
 
   }
 
-  equipPokemon(teammate){
+  equipPokemon_ID(name){
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        let resp = this.responseText
+        let data = JSON.parse(resp)
+        var chosenPoke = new Pokemon(data.name)
+        chosenPoke.name = data.name
+        chosenPoke.hp = data.stats[5].base_stat
+        chosenPoke.attack = data.stats[4].base_stat
+        chosenPoke.defense = data.stats[3].base_stat
+        console.log(chosenPoke)
 
-
-
-
-    this.team.push(teammate)
-    console.log(`${teammate.name} has joined your party`)
+        for (let i in data.abilities){
+          let a = data.abilities[i].ability.name
+          a.replace('-',' ').charAt(0).toUpperCase()
+          chosenPoke.abilities.push(a)
+        }
+      }
+    }
+    xhttp.open("GET", `https://fizal.me/pokeapi/api/v2/id/${name}.json`, true);
+    xhttp.send();
   }
+  //-------------------
   all() {
     return this.team
   }
+  //------------------
   get(name){
     for (let i = 0; i < 5;i++){
       if (this.team[i] == name){
         return this.team[i]
       }
     }
-
   }
 }
 //
+
 class Pokemon {
-  constructor(name){
+  constructor(name,hp,attack,defense){
     this.name = name
     this.hp = hp
     this.attack = attack
@@ -37,46 +55,37 @@ class Pokemon {
 }
 
 // ----------- TRUE API POKEDEX -----------------
-
+//
 abilityList = document.getElementById('ability-list')
+statsList = document.getElementById('stats-list')
 
-queryPokemonAPI = async () => {
-  pokeNum = prompt("Enter pokemon ID: ")
-const url = `https://fizal.me/pokeapi/api/v2/id/${pokeNum}.json`
-  fetch(url)
-    .then( resp => {
-      return resp.json()
-    })
-    .then( data => {
-        console.log(data)
-         let pokemon = {
-          name: data.name,
-          hp: data.stats[5].base_stat,
-          atk: data.stats[4].base_stat,
-          def: data.stats[3].base_stat,
-          abilities: []
-        }
-        for (let i in data.abilities){
-        console.log(data.abilities[i].ability.name)
-        // abilities.push(data.abilities[i].ability.name)
-
-        let li = document.createElement('li')
-        li.innerText =`${data.abilities[i].ability.name}`
-        li.style.color = "white"
-        abilityList.appendChild(li)
+// queryPokemonAPI = async () => {
+  // pokeNum = prompt("Enter pokemon ID: ")
+//   let randPoke = Math.floor(Math.random() * 100) + 1
+// const url = `https://fizal.me/pokeapi/api/v2/id/${randPoke}.json`
+//   fetch(url)
+//     .then( resp => {
+//       return resp.json()
+//     })
+//     .then( data => {
+//         console.log(data)
+//          let pokemon = new Pokemon(data.name)
+//           pokemon.name = data.name
+//           pokemon.hp = data.stats[5].base_stat
+//           pokemon.attack = data.stats[4].base_stat
+//           pokemon.defense = data.stats[3].base_stat
+//
+//
+//
+//
 
 
-        }
-        // pokemon['name'] = data.name
-          // console.log(data)
-        // console.log(Pokemon)
 
-        })
 
-        // box = document.createElement()
-}
-queryPokemonAPI()
-
+//       )
+//
+//         // box = document.createElement()
+// }
 
 // ------------ ANIMATIONS -------------------
 function start(){
@@ -109,37 +118,6 @@ function start(){
 //-----------------------------------------------------
 start()
 
-// let t1 = new Trainer('Red')
+let t1 = new Trainer('Red')
 // let poke1 = new Pokemon('bulbasaur')
 // let poke2 = new Pokemon('squirtle')
-
-//
-// // -----------API STUFF--------------
-
-/*
-// Create a request variable and assign a new XMLHttpRequest object to it.
-var request = new XMLHttpRequest()
-
-// Open a new connection, using the GET request on the URL endpoint
-for (i = 1; i <= 20;i++){
-  request.open('GET', `https://fizal.me/pokeapi/api/v2/pokemon`, true)
-
-  request.onload = function() {
-    // Begin accessing JSON data here
-    var data = JSON.parse(this.name)
-
-    if (request.status >= 200 && request.status < 400){
-    data.forEach(pokemon =>{
-    // Log each movie's title
-    console.log(pokemon.name)
-  })
-  } else {
-      console.log('error')
-    }
-  }
-}
-
-// Send request
-request.send()
-
-*/
